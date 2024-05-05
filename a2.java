85315017204d6e8e28904df870919b5d0b8983f8
Pratik-9324 <160480812+Pratik-9324@users.noreply.github.com>
2024-05-05 23:49:54 +0530

    Add files via upload

---
+++ a2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class a2 {
+    public static void main(String[] args)
+    {
+        int arr[]={10,20,30,40};
+        System.out.println("Integer Array:"+Arrays.stream(arr));
+    }
+}
